0fa2a618d52549edde4af01d36038bb795991069
sergey basharin <nuzqot@gmail.com>
2022-01-28 14:57:06 +0300

    исправил ошибки, которые выдал линтер

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,9 @@
 import readlineSync from 'readline-sync';
-console.log('Welcome to the Brain Games!');
-export const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!' )
+
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName} !`);
+};
+
+export default greeting;
